cdd5940b20abb6189c716658aca91f326fed7bdb
BuildTools <unconfigured@null.spigotmc.org>
2023-02-25 10:22:05 +0400

    fas

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -24,37 +24,19 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-    public String currentDateandTime = "-1";
-    public String foinString = " ";
-    private MyDraw myDraw;
-    private DrawThread drawThread;
-    public static final String APP_PREFERENCES = "mysettings";
-    public static final String APP_PREFERENCES_NAME = "foin";
-    //SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-    //SharedPreferences.Editor editor = mSettings.edit();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        //if(mSettings.contains(APP_PREFERENCES_NAME)) {
-        //    foinString = (mSettings.getString(APP_PREFERENCES_NAME, ""));
-        //}
         setContentView(new MyDraw(this));
         //setContentView(R.layout.shopskin);
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //editor.putInt(APP_PREFERENCES_NAME, foin);
-        ///editor.apply();
     }
     @Override
     protected void onStop() {
         super.onStop();
-        //SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-        //SharedPreferences.Editor editor = mSettings.edit();
-        //editor.putInt(APP_PREFERENCES_NAME, foin);
-        //editor.apply();
     }
 }
